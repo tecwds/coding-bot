6a5646b4dc3db9fd8c14e13a0f21ec235aa181fd
tecwds <tecwds@163.com>
2024-06-29 16:23:47 +0800

    mod: server.rs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,13 +4,13 @@ use coding_bot::model::event::{Event, EventHandler};
 use rocket::{http::Status, post, response::content, routes, serde::json::Json};
 
 #[post("/", data = "<data>")]
-async fn handle_post(data: Json<Event>) -> Result<content::RawText<String>, Status> {
-    println!("{data:?}");
+async fn handle_post(data: String) -> Result<content::RawText<String>, Status> {
+    println!("{data}");
 
-    EventHandler::MessageHandler(data.0)
-        .handler()
-        .await
-        .unwrap();
+    // EventHandler::MessageHandler(data.0)
+    //     .handler()
+    //     .await
+    //     .unwrap();
 
     Ok(content::RawText("Received data".to_string()))
 }
